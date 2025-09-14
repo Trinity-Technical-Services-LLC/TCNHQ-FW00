0469f52f6ff40884d51a7125ff7b10cd27404a4b
Smarter  Harder <33955773+NWarila@users.noreply.github.com>
2025-09-13 20:24:49 -0400

    Changes to be committed: 	modified:   03-providers.tf 	modified:   09-resources.tf

--- 03-providers.tf
+++ 03-providers.tf
@@ -3,4 +3,5 @@ provider "opnsense" {
   url        = var.fw02_url
   api_key    = var.fw02_api_key
   api_secret = var.fw02_api_secret
+  allow_insecure = true
 }
--- 09-resources.tf
+++ 09-resources.tf
@@ -1,8 +1,14 @@
 
 resource "opnsense_interfaces_vlan" "vlan_100" {
+
+  # Explicitly define the provider to use
+  provider = opnsense.TCNHQ-FW02
+
+  # vLan Configuration
   description = "VLAN 100 - Gateway"
   device      = "vlan0.100"
   parent      = "lagg0"
   priority    = "0"
   tag         = "100"
+
 }
